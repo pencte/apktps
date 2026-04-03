0134ad260cd96c233acd252c8af043a2fcc2383c
pencte <zakyyulyansyah@gmail.com>
2026-04-03 21:43:06 +0700

    Simplify server data handler response

--- api/server_data.js
+++ api/server_data.js
@@ -1,11 +1,4 @@
-export default async function handler(req, res) {
-  try {
-    const response = await fetch("http://139.99.72.27:80/server_data.php");
-    const text = await response.text();
-
-    res.setHeader("Content-Type", "text/plain");
-    res.status(200).send(text);
-  } catch (err) {
-    res.status(500).send("server|offline\nport|17091\ntype|1");
-  }
+export default function handler(req, res) {
+  res.setHeader("Content-Type", "text/plain");
+  res.send("server|1.1.1.1\nport|17091\ntype|1");
 }
