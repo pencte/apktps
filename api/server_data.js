bb620f9d7cdcfec75680b1d561dd374438a90ea1
pencte <zakyyulyansyah@gmail.com>
2026-04-03 23:18:18 +0700

    Update server_data.js

--- api/server_data.js
+++ api/server_data.js
@@ -1,4 +1,16 @@
 export default function handler(req, res) {
   res.setHeader("Content-Type", "text/plain");
-  res.send("server|1.1.1.1\nport|17091\ntype|1");
+
+  const response = `
+server|139.99.72.27
+port|17091
+type|1
+type|2
+loginurl|fff.albin-url.my.id:3000
+#maint|Server currently change hosting, please join discord.gg/gtps15 to get the latest host.
+meta|XinPS
+RTENDMARKERBS1001
+`;
+
+  res.status(200).send(response);
 }
