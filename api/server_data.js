76ae61b19dec60d0e48d9c2069e4c92859a3bd18
pencte <zakyyulyansyah@gmail.com>
2026-04-03 21:36:50 +0700

    Update server_data.js

--- api/server_data.js
+++ api/server_data.js
@@ -1,7 +1,11 @@
 export default async function handler(req, res) {
-  const response = await fetch("http://139.99.72.27:17091/server_data.php");
-  const text = await response.text();
+  try {
+    const response = await fetch("http://139.99.72.27:80/server_data.php");
+    const text = await response.text();
 
-  res.setHeader("Content-Type", "text/plain");
-  res.send(text);
+    res.setHeader("Content-Type", "text/plain");
+    res.status(200).send(text);
+  } catch (err) {
+    res.status(500).send("server|offline\nport|17091\ntype|1");
+  }
 }
